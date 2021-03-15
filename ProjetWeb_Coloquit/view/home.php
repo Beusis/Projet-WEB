4925e52a559ca276a9161e8bd13940d06ba07a39
Axwells <Axel.PITTET@cpnv.ch>
2021-03-15 10:24:27 +0100

    Update Offers

--- ProjetWeb_Coloquit/view/home.php
+++ ProjetWeb_Coloquit/view/home.php
@@ -4,47 +4,45 @@ ob_start();
 ?>
     <div id="offresMenu">
         <div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+
             <div class="col">
-                <a href="index.php?action=displayOffer">
-                    <button class="border-0 p-0">
-                        <div class="card h-100">
-                            <img src="view/content/img/colocLogo.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural
-                                    lead-in
-                                    to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </button>
-                </a>
-            </div>
-            <div class="col">
-                <a href="index.php?action=displayOffer">
-                    <button class="border-0 p-0">
-                        <div class="card h-100">
-                            <img src="view/content/img/Landscape-Color.jpg" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a short card.</p>
-                            </div>
-                        </div>
-                    </button>
-                </a>
+                <div class="card h-100">
+                    <img src="view/content/img/colocLogo.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural
+                            lead-in
+                            to additional content. This content is a little bit longer.</p>
+                        <a href="index.php?action=displayOffer">
+                            <button class="btn btn-secondary">Voir en détail</button>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="col">
                 <div class="card h-100">
-                    <a href="index.php?action=displayOffer">
-                        <button class="border-0 p-0">
-                            <img src="view/content/img/colocLogo.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural
-                                    lead-in to
-                                    additional content.</p>
-                            </div>
-                        </button>
-                    </a>
+                    <img src="view/content/img/Landscape-Color.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a short card.</p>
+                        <a href="index.php?action=displayOffer">
+                            <button class="btn btn-secondary">Voir en détail</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="view/content/img/colocLogo.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural
+                            lead-in to
+                            additional content.</p>
+                        <a href="index.php?action=displayOffer">
+                            <button class="btn btn-secondary">Voir en détail</button>
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="col">
@@ -52,8 +50,12 @@ ob_start();
                     <img src="view/content/img/Landscape-Color.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        <p class="card-text">This is a longer card with supporting text below as a natural
+                            lead-in to
                             additional content. This content is a little bit longer.</p>
+                        <a href="index.php?action=displayOffer">
+                            <button class="btn btn-secondary">Voir en détail</button>
+                        </a>
                     </div>
                 </div>
             </div>
